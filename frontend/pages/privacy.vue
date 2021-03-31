725f2026c476881ede8cf57d48d9bb57359a8cd0
Sergio D Munoz Sierra <sdmunozsierra@miners.com>
2021-03-30 21:59:34 -0400

    added home privacy and terms

---
+++ frontend/pages/privacy.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container fluid class="pa-0 ma-0 fill-height">
+    <iframe
+      class="clickup-embed clickup-dynamic-height"
+      src="https://doc.clickup.com/p/88hjt-210/cleta-privacy-policy"
+      onwheel=""
+      width="100%"
+      height="100%"
+    ></iframe>
+  </v-container>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+iframe {
+  border: 0;
+}
+</style>
