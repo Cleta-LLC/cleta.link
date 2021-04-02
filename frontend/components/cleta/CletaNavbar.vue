d21e3e9bffe705ce5fbc833243a107ce8013c6fe
Sergio D Munoz Sierra <sdmunozsierra@miners.com>
2021-04-01 21:46:14 -0400

    Added sidebar as component

---
+++ frontend/components/cleta/CletaNavbar.vue
@@ -0,0 +1,134 @@
+<template>
+  <base-nav
+    :v-model="showMenu"
+    type="light"
+    :transparent="true"
+    menu-classes="justify-content-end"
+    class="navbar-horizontal navbar-main"
+    expand="lg"
+    :show="true"
+  >
+    <div slot="brand" class="navbar-wrapper">
+      <nuxt-link class="navbar-brand" to="/">
+        <img src="img/cleta/white.png" alt="Cleta Logo white" />
+      </nuxt-link>
+    </div>
+
+    <template>
+      <div class="navbar-collapse-header">
+        <div class="row">
+          <div class="col-6 collapse-brand">
+            <nuxt-link to="/">
+              <img src="img/cleta/black.png" alt="Cleta Logo green" />
+            </nuxt-link>
+          </div>
+          <div class="col-6 collapse-close">
+            <button
+              type="button"
+              class="navbar-toggler"
+              @click="showMenu = false"
+            >
+              <span></span>
+              <span></span>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <nuxt-link to="/explore" class="nav-link">
+            <span class="nav-link-inner--text">Explore</span>
+          </nuxt-link>
+        </li>
+        <li class="nav-item">
+          <!-- Not Authenticated go to login else go to profile dashboard -->
+          <div v-if="!$auth.isAuthenticated">
+            <nuxt-link to="/login" class="nav-link">
+              <span class="nav-link-inner--text">Login</span>
+            </nuxt-link>
+          </div>
+          <div v-else>
+            <nuxt-link to="/profile" class="nav-link">
+              <span class="nav-link-inner--text">Profile Dashboard</span>
+            </nuxt-link>
+          </div>
+        </li>
+      </ul>
+
+      <hr class="d-lg-none" />
+      <ul class="navbar-nav align-items-lg-center ml-lg-auto">
+        <li class="nav-item">
+          <a
+            class="nav-link nav-link-icon"
+            href="https://www.facebook.com/cleta.io/"
+            target="_blank"
+            rel="noopener"
+            aria-label="Facebook"
+          >
+            <i class="fab fa-facebook-square"></i>
+            <span class="nav-link-inner--text d-lg-none">Facebook</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a
+            class="nav-link nav-link-icon"
+            href="https://www.instagram.com/cleta.io/"
+            target="_blank"
+            rel="noopener"
+            aria-label="Instagram"
+          >
+            <i class="fab fa-instagram"></i>
+            <span class="nav-link-inner--text d-lg-none">Instagram</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a
+            class="nav-link nav-link-icon"
+            href="https://github.com/Cleta-LLC/cleta.link/"
+            target="_blank"
+            rel="noopener"
+            aria-label="Github"
+          >
+            <i class="fab fa-github"></i>
+            <span class="nav-link-inner--text d-lg-none">Github</span>
+          </a>
+        </li>
+        <li class="nav-item d-none d-lg-block ml-lg-4">
+          <NuxtLink to="/login" class="btn btn-secondary">Login</NuxtLink>
+        </li>
+      </ul>
+    </template>
+  </base-nav>
+</template>
+<script>
+import BaseNav from '~/components/argon-core/Navbar/BaseNav.vue'
+
+export default {
+  components: {
+    BaseNav
+  },
+  props: {
+    backgroundColor: {
+      type: String,
+      default: 'black'
+    },
+    show: 'true'
+  },
+  data() {
+    return {
+      showMenu: true
+    }
+  },
+  computed: {
+    title() {
+      return `${this.$route.name} Page`
+    }
+  }
+}
+</script>
+<style lang="scss">
+.auth-layout {
+  min-height: 100vh;
+}
+</style>
---
+++ frontend/components/cleta/CletaSidebar.vue
@@ -0,0 +1,90 @@
+<template>
+  <side-bar>
+    <template slot-scope="props" slot="links">
+      <sidebar-item
+        :link="{
+          name: 'NFT Markeplace',
+          icon: 'ni ni-shop text-primary'
+        }"
+      >
+        <sidebar-item
+          :link="{ name: 'Create a NFT', path: '/mint' }"
+        ></sidebar-item>
+        <sidebar-item
+          :link="{ name: 'Sell a NFT', path: '/sell' }"
+        ></sidebar-item>
+        <sidebar-item
+          :link="{ name: 'Transfer a NFT', path: '/transfer' }"
+        ></sidebar-item>
+      </sidebar-item>
+
+      <sidebar-item
+        :link="{
+          name: 'Explore',
+          icon: 'ni ni-world text-orange',
+          path: '/discover'
+        }"
+      >
+      </sidebar-item>
+
+      <sidebar-item
+        :link="{
+          name: 'Wallet',
+          icon: 'ni ni-credit-card text-success',
+          path: '/wallet'
+        }"
+      >
+      </sidebar-item>
+
+      <sidebar-item
+        :link="{
+          name: 'Settings',
+          icon: 'ni ni-settings text-grey',
+          path: '/settings'
+        }"
+      >
+      </sidebar-item>
+    </template>
+
+    <template slot="links-after">
+      <hr class="my-3" />
+      <h6 class="navbar-heading p-0 text-muted">Resources</h6>
+
+      <ul class="navbar-nav mb-md-3">
+        <li class="nav-item">
+          <a
+            class="nav-link"
+            href="https://cleta.io/docs"
+            target="_blank"
+            rel="noopener"
+          >
+            <i class="ni ni-spaceship"></i>
+            <span class="nav-link-text">Getting started</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a
+            class="nav-link"
+            href="https://cleta.io/donate"
+            target="_blank"
+            rel="noopener"
+          >
+            <i class="ni ni-satisfied"></i>
+            <span class="nav-link-text">Donate</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a
+            class="nav-link"
+            href="https://cleta.io/logout"
+            target="_blank"
+            rel="noopener"
+          >
+            <i class="ni ni-user-run"></i>
+            <span class="nav-link-text">Logout</span>
+          </a>
+        </li>
+      </ul>
+    </template>
+  </side-bar>
+</template>
